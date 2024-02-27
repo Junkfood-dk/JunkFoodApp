8f8589fa28da1850c7214cbd6b9b870bdabcf581
Markus Brandt Højgaard <mbrh@itu.dk>
2024-02-27 12:41:16 +0100

    test workflow with minor change
    Co-authored-by: Omar <omse@itu.dk>
    Co-authored-by: Markus <mbrh@itu.dk>
    Co-authored-by: Lukas <lupa@itu.dk>
    Co-authored-by: Jonas <kram@itu.dk>
    Co-authored-by: Johan <jsbe@itu.dk>
    Co-authored-by: Lauritz <lana@itu.dk>
    Co-authored by: Allan <asia@itu.dk>

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have punched the button this many times:',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
