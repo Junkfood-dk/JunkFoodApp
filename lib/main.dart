f9410d51716f5c8c08e2384fbb53f0c7e04864bd
Lukas <lupa@itu.dk>
2024-03-11 12:53:00 +0100

    Move button for language selection to a component.
    The functionality of the button is moved to its own component
    
    Co-authored-by: Jonas <kram@itu.dk>
    Co-authored-by: Johan <jsbe@itu.dk>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:userapp/components/language_dropdown_component.dart';
 import 'package:userapp/model/language.dart';
 import 'model/locale.dart';
 
@@ -105,24 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(AppLocalizations.of(context)!.homePageTitle),
-        actions: <Widget>[
-          Consumer<LocaleModel>(
-            builder: (context, localeModel, child) => PopupMenuButton<Language>(
-              onSelected: (Language language) {
-                localeModel.set(Locale(language.languageCode));
-              },
-              icon: const Icon(Icons.language),
-              itemBuilder: (BuildContext context) {
-                List<PopupMenuEntry<Language>> menuItems =
-                  Language.languageList().map((e) {
-                    return PopupMenuItem<Language>(
-                      value: e,
-                      child: Text(e.name));
-                  }).toList();
-                  return menuItems;
-              }),
-            ),
-        ],
+        actions: <Widget>[LanguageDropdown()],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
