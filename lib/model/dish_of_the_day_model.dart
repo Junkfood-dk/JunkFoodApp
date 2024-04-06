0efb59f2794123582dd1162028a9f04f8b02d9da
Allan <asia@itu.dk>
2024-04-06 19:58:27 +0200

    connects to db

---
+++ lib/model/dish_of_the_day_model.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:userapp/model/dish_model.dart';
+
+class DishOfTheDayModel extends ChangeNotifier {
+  final SupabaseClient database;
+  DishOfTheDayModel({required this.database});
+  DishModel? _dishOfTheDay;
+
+  Future<void> fetchDishOfTheDay() async {
+    Future.microtask(() async {
+      var response = await database
+          .from("Dish_Schedule")
+          .select()
+          .filter("date", "eq", DateTime.now().toIso8601String());
+      if (response.isNotEmpty) {
+        var dishOfTheDay = await database
+            .from("Dishes")
+            .select()
+            .filter("id", "eq", response[0]["id"]);
+        _dishOfTheDay = DishModel.fromJson(dishOfTheDay[0]);
+      } else {
+        _dishOfTheDay = null;
+      }
+      notifyListeners();
+    });
+  }
+
+  DishModel get dishOfTheDay {
+    if (_dishOfTheDay != null) {
+      return _dishOfTheDay!;
+    } else {
+      return DishModel(title: "There is no dish of the day");
+    }
+  }
+
+  Future<bool> get hasDishOfTheDay async {
+    await fetchDishOfTheDay();
+    return _dishOfTheDay != null;
+  }
+
+  Future<int> postDishOfTheDay(
+      String title, String description, int calories, String imageUrl) async {
+    DishModel newDish = DishModel(
+        title: title,
+        description: description,
+        calories: calories,
+        imageUrl: imageUrl);
+    var row = await database.from("Dishes").insert(newDish).select("id");
+    var id = row[0]['id'];
+    await database.from("Dish_Schedule").insert(
+        {'id': id, 'date': DateTime.now().toIso8601String()}).select("id");
+    return id;
+  }
+}
---
+++ lib/model/show_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:supabase/src/supabase_client.dart';
+import 'package:userapp/components/language_dropdown_component.dart';
+import 'package:userapp/model/dish_of_the_day_model.dart';
+import 'package:userapp/model/locale.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
+class ShowPage extends StatelessWidget {
+  const ShowPage({super.key, required SupabaseClient database});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<LocaleModel>(
+      builder: (context, localeModel, child) => Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context)!.homePageTitle),
+          actions: [LanguageDropdown()],
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: Consumer<DishOfTheDayModel>(builder: (context, state, _) {
+            return FutureBuilder(
+                future: state.hasDishOfTheDay,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  } else {
+                    if (!snapshot.data!) {
+                      return Text("1");
+                    } else {
+                      return Text("2");
+                    }
+                  }
+                });
+          }),
+        ),
+      ),
+    );
+  }
+}
