e13775165b6d41417aae8467c4e665bdf788445c
lrtzndrsn <lana@itu.dk>
2024-04-09 11:13:15 +0200

    Change ratio of picture in dish_display_component.dart
    Updating the ratio such that there is no bottom overflow.
    
    Co-authored-by: Lukas <lupa@itu.dk>

--- lib/model/dish_model.dart
+++ lib/model/dish_model.dart
@@ -28,16 +28,16 @@ class DishModel {
 
   static DishModel fromJson(Map<String, dynamic> input) {
     return DishModel(
-        id: input.containsKey("dish_id")
-          ? input["dish_id"]
+      id: input.containsKey("id")
+          ? input["id"]
           : throw Exception("No id provided"),
-        title: input.containsKey("title")
-            ? input["title"]
-            : throw Exception("No title provided"),
-        description:
-            input.containsKey("description") ? input["description"] : "",
-        calories: input.containsKey("calories") ? input["calories"] : 0,
-        imageUrl: input.containsKey("image") ? input["image"] : "",
-        dishType: input.containsKey("dish_type") ? input["dish_type"] : "",);
+      title: input.containsKey("title")
+          ? input["title"]
+          : throw Exception("No title provided"),
+      description: input.containsKey("description") ? input["description"] : "",
+      calories: input.containsKey("calories") ? input["calories"] : 0,
+      imageUrl: input.containsKey("image") ? input["image"] : "",
+      dishType: input.containsKey("dish_type") ? input["dish_type"] : "",
+    );
   }
 }
