6aed4badc9f0f650aed099d18e6d9bf53cdf4579
Allan <asia@itu.dk>
2024-04-14 14:56:58 +0200

    new textFinder

--- test/dont_show_dish_test.dart
+++ test/dont_show_dish_test.dart
@@ -1,25 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:userapp/components/no_dish_component.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 
 void main() {
-//Test if locale changes when language is selected in dropdown-menu
   testWidgets('no_dish_component_displays_no_dish_text_when_no_dishes',
       (WidgetTester tester) async {
-    //Arrange
-    await tester
-        .pumpWidget(MaterialApp(home: NoDishComponent()));
+    // Arrange
+    await tester.pumpWidget(MaterialApp(home: NoDishComponent()));
 
-    //Act
-    final noDishText = AppLocalizations.of(context as BuildContext)?.noDishText;
-    final textFinder = find.text(noDishText!);
+    // Act
+    final textFinder =
+        find.text('Todays menu is not ready yet. Please check back later.');
+        //Dagens ret er ikke offentliggjort endnu. Tjek siden senere.
 
-    //Assert
-    expect(textFinder, find.text("Todays menu is not ready yet. Please check back later."));
+    // Assert
+    expect(textFinder, findsOneWidget);
   });
-}
-
-mixin context {
 }
