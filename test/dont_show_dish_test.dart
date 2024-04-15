a4b30fbc955a76f3a4a46f57de7863893131aa96
Allan <asia@itu.dk>
2024-04-15 11:31:40 +0200

    add locale to show text

--- test/dont_show_dish_test.dart
+++ test/dont_show_dish_test.dart
@@ -1,17 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:userapp/components/no_dish_component.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 void main() {
   testWidgets('no_dish_component_displays_no_dish_text_when_no_dishes',
       (WidgetTester tester) async {
     // Arrange
-    await tester.pumpWidget(MaterialApp(home: NoDishComponent()));
+    await tester.pumpWidget(MaterialApp(
+      localizationsDelegates: const[AppLocalizations.delegate],
+      locale: Locale('en'),
+      home: NoDishComponent()));
 
     // Act
     final textFinder =
         find.text('Todays menu is not ready yet. Please check back later.');
-        //Dagens ret er ikke offentliggjort endnu. Tjek siden senere.
 
     // Assert
     expect(textFinder, findsOneWidget);
