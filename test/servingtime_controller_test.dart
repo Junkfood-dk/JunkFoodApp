207a87d87687cd5be77e5580b6af0f8fce12a904
Lukas <lupa@itu.dk>
2024-04-23 11:53:03 +0200

    Remove mock

--- test/servingtime_controller_test.dart
+++ test/servingtime_controller_test.dart
@@ -1,10 +1,7 @@
 import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:userapp/ui/controllers/servingtime_controller.dart';
-
-import 'servingtime_controller_test.mocks.dart';
 
 ProviderContainer createContainer({
   ProviderContainer? parent,
