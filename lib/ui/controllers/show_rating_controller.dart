1705d0ec8d7e0c0a815fcb17da311cc60fadecb0
Markus Brandt Højgaard <mbrh@itu.dk>
2024-04-27 22:42:44 +0200

    Fix this please

---
+++ lib/ui/controllers/show_rating_controller.dart
@@ -0,0 +1,9 @@
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+@riverpod 
+class ShowRatingController extends _$ShowRatingController{
+  bool build() {
+    return false;
+  }
+}
