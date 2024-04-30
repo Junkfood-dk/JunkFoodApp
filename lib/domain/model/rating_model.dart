855a609d5272638fd37779bcbe7b9fb1588a09a9
Lukas <lupa@itu.dk>
2024-04-30 12:30:34 +0200

    Rating works. But rls is problem.
    Co-authored-by: Lauritzs <lana@itu.dk>

--- lib/domain/model/rating_model.dart
+++ lib/domain/model/rating_model.dart
@@ -1,20 +1,18 @@
 class RatingModel {
-  int id;
   int rating;
-  String user_guid; // for only accepting one answer per user per dish
+  // String user_guid; // for only accepting one answer per user per dish
   int dish_id;
 
   RatingModel(
-      {this.id = -1,
+      {
       required this.rating,
-      this.user_guid = "placeholder",
+      // this.user_guid = "placeholder",
       required this.dish_id});
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
       'rating': rating,
-      'future_user_guid': user_guid,
+      // 'future_user_guid': user_guid,
       'dish_id': dish_id,
     };
   }
