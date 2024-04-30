e051e1e8b9fef420a154a630a95125ee924f152b
Lukas <lupa@itu.dk>
2024-04-30 11:09:16 +0200

    Finish up rating repository.

--- lib/data/rating_repository.dart
+++ lib/data/rating_repository.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/foundation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:userapp/data/database.dart';
 import 'package:userapp/data/interface_rating_repository.dart';
 import 'package:userapp/domain/model/rating_model.dart';
+
+part 'rating_repository.g.dart';
 
 class RatingRepository implements IRatingRepository {
   SupabaseClient database;
@@ -23,4 +27,11 @@ class RatingRepository implements IRatingRepository {
       throw Exception("Failed to save new rating: $error");
     }
   }
+
+  
+}
+
+@riverpod
+IRatingRepository ratingRepository(RatingRepositoryRef ref) {
+  return RatingRepository(database: ref.read(databaseProvider));
 }
---
+++ lib/data/rating_repository.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'rating_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$ratingRepositoryHash() => r'005314b321cb919b4b6d7eb27b296c746dc5bfb5';
+
+/// See also [ratingRepository].
+@ProviderFor(ratingRepository)
+final ratingRepositoryProvider =
+    AutoDisposeProvider<IRatingRepository>.internal(
+  ratingRepository,
+  name: r'ratingRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$ratingRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RatingRepositoryRef = AutoDisposeProviderRef<IRatingRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
