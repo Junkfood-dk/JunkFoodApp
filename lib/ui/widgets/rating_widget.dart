12149bde4f8738f8143bef05d172733b42bee4da
Lukas <lupa@itu.dk>
2024-04-30 14:05:32 +0200

    Merge fix
    Co-authored-by: Lauritzs <lana@itu.dk>

--- lib/ui/widgets/rating_widget.dart
+++ lib/ui/widgets/rating_widget.dart
@@ -66,7 +66,7 @@ class RatingWidget extends ConsumerWidget {
       Container(
           width: MediaQuery.of(context).size.width * 0.9,
           height: MediaQuery.of(context).size.height * 0.08,
-          child: gradiantButton(
+          child: GradiantButton(
               child: ButtonText(
                 text: AppLocalizations.of(context)!.ratingContinue,
               ),
