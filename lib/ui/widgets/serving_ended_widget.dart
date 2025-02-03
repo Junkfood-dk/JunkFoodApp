44e6d231bb1299a775f2110b2a2d9bfa66ca303a
e138206 <jakob.skov@mastercard.com>
2025-02-03 09:26:27 +0100

    Add refresh indicator to no dish and serving ended

--- lib/ui/widgets/serving_ended_widget.dart
+++ lib/ui/widgets/serving_ended_widget.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:junkfood/ui/controllers/dish_of_the_day_controller.dart';
 import 'package:junkfood/ui/widgets/logo_image.dart';
+import 'package:junkfood/utilities/widgets/sized_box_ext.dart';
 import 'package:junkfood/utilities/widgets/text_wrapper.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ServingEndedWidget extends StatelessWidget {
+class ServingEndedWidget extends ConsumerWidget {
   const ServingEndedWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Material(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: RefreshIndicator(
+          onRefresh: () async {
+            ref
+                .read(dishOfTheDayControllerProvider.notifier)
+                .refetchDishOfTheDay();
+          },
+          child: ListView(
             children: [
+              SizedBoxExt.sizedBoxHeight24,
               const LogoImage(),
               TitleLargeText(
                 text: AppLocalizations.of(context)!.servingHasEndedText,
